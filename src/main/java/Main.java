8ccbc5fb14e5825864aaa2373fa674b2d593ebbf
Jakub <wasjakub3237@gmail.com>
2019-08-15 18:42:58 +0200

    Final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,6 +18,7 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Color Picker");
+        primaryStage.getIcons().add(new Image("d3j5pdx-474f27e6-fae7-4784-a4af-23245683a869.png"));
         primaryStage.setResizable(false);
         primaryStage.setAlwaysOnTop(false);
         primaryStage.show();
--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,6 +1,7 @@
 import com.jfoenix.controls.JFXButton;
 
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.paint.Color;
 
 import java.awt.*;
@@ -17,11 +18,30 @@ public class Model {
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
     }
-    public static void color(Color color,JFXButton button1,JFXButton button2, JFXButton button3){
-        String colorName = "#"+color.toString().substring(2,8);
 
+    public static void color(Color color, final JFXButton button1, final JFXButton button2, final JFXButton button3){
+        String colorName = "#"+color.toString().substring(2,8);
         button1.setStyle("-fx-background-color: " +colorName+";");
         button1.setText(colorName.toUpperCase());
+
+
+        button1.textProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if (!newValue.equals(oldValue)){
+                       button1.setStyle("-fx-background-color: "+ newValue+";");
+                       button1.setText(newValue.toUpperCase());
+                       button2.setStyle("-fx-background-color: "+ oldValue+";");
+                       button2.setText(oldValue.toUpperCase());
+                    }
+                    button2.textProperty().addListener(new ChangeListener<String>() {
+                        public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                            if (!newValue.equals(oldValue)) {
+                                button3.setStyle("-fx-background-color: "+ oldValue+";");
+                                button3.setText(oldValue.toUpperCase());
+                            }
+                        }});
+            }
+        });
 
 
     }
